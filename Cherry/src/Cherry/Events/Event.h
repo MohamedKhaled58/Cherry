1a339e3a3523b084a475803f0c36a6ccbf4aef6c
MohamedKhaled <m.khaled64.mmk@gmail.com>
2025-07-25 04:53:40 +0300

    Avoid using std::function in event dispatcher for performance

--- Cherry/src/Cherry/Events/Event.h
+++ Cherry/src/Cherry/Events/Event.h
@@ -57,22 +57,21 @@ namespace Cherry {
 		std::string m_event;
 	};
 
-	class  EventDispatcher
+	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
 	public:
 		EventDispatcher(Event& event)
-			:m_Event(event) { }
+			: m_Event(event) 
+		{
+		}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		// F will be deduced by the compiler
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_handled = func(*(T*) & m_Event);
-				//m_Event.m_handled = func(static_cast<T&>(m_Event));
-
+				m_Event.Handled = func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
@@ -80,6 +79,7 @@ namespace Cherry {
 	private:
 		Event& m_Event;
 	};
+
 
 	inline std::ostream& operator<<(std::ostream& os, const Event& e)
 	{
